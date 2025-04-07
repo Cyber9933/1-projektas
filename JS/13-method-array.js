c9e19dd0637a268030c08a27301744b68b9629df
Lina <linate.pa@gmail.com>
2025-04-07 09:54:53 +0300

    method

--- JS/13-method-array.js
+++ JS/13-method-array.js
@@ -92,3 +92,80 @@ console.log(n12);
 
 console.log('----5-----');
 
+const numbers2=[10,2,8,4,6];;
+console.log(numbers2);
+numbers2.push(2);
+console.log(numbers2);
+
+numbers2.pop(); //paskutinis elementas ismetamas
+numbers2.pop(); //paskutinis elementas ismetamas
+console.log(numbers2);
+
+const g1=numbers2.pop(); //paskutinis elementas ismetamas
+// ne tik pasalina bet ir grazina ka pasalino
+const g2=numbers2.pop();
+console.log(numbers2, g1, g2);
+
+numbers1.unshift(1); //prideda nauja elementa i saraso pradzia
+numbers2.unshift(2); //prideda nauja elementa i saraso pradzia
+console.log(numbers2);
+
+numbers2.shift(); //pirmas elementas ismetamas
+const g3=numbers2.shift(); //pirmas elementas ismetamas
+const g4=numbers2.shift(); //pirmas elementas ismetamas
+console.log(numbers2, g3, g4);
+
+console.log('----6-----');
+// ar reiksme egzistuoja sarase?
+
+
+const magic=[11,3,4,5,]
+console.log(magic.includes(5));
+console.log(magic.includes(44));
+
+console.log(magic.indexOf(5));
+console.log(magic.indexOf(44));
+
+
+console.log('----7-----');
+//padeti sukurti is saraso paimama teksta
+const text=['namas','petras','labas','rytas'];
+const home=`namuose yra like: ${text.join()}.`;
+console.log(home);
+
+//dazniausiai naudojamas kaip paskutinis irankis apjungti sarasa i viena teksta;
+
+console.log('------8--------');
+//tik masyve gali naudoti reversa, stringe jis netinka
+
+console.log(text);
+text.reverse();
+console.log(text);
+
+console.log('------9--------');
+// concat apjungia masyvus
+//apjungimo kiekis neribojamas pvz su push ribojamas tik iki2 masyvu
+
+
+const c1=[1,11];
+const c2=[2,22];
+const c3=[3,33];
+const c4=[4,44];
+
+const c12=c1.concat(c2);
+console.log(c12);
+
+const c14=c3.concat(c4);
+console.log(c14);
+
+const c1234=c1.concat(c2,c3,c4);
+console.log(c1234);
+
+//galima pernaudoti ta pati masyva
+const c111=c1.concat(c1,c1,c1);
+console.log(c111);
+
+console.log('------10--------');
+
+
+
---
+++ task/names-split-join.js
@@ -0,0 +1,54 @@
+/*
+
+Funcija gauna sakini, kuris atrodo taip:
+"Studentai: jonas, Maryte, petTras, ONA."
+
+F-jos darbas grazinti teisingai perrasyta sakini.
+t.y istaisyti vardus:
+pvz"Studentai:Jonas, Maryte, Petras, Ona."
+*/
+
+
+/*
+-atsistirti vardsu is orginalaus teksto
+-konvertuojame teksta i mazasias raides
+-issiskaidyti individuoalius vardus i masyva
+-ciklas, eina per individualius vardus:
+//-pirmas raide didziosios raides
+     - pasiima pirma raide,  .split
+     -ja padidina .uperCase
+     -apjungi i galutini normalu varda 
+-paganimae galitini reikiama sakini .join
+*/
+
+function correctSentence(text) {
+    const parts=text.split(':');
+  
+    const names=parts[1].slice(0,-1).toLowerCase().split(', ');
+
+      console.log(parts);
+      console.log(names);
+      for (let i=0; i<names.legth; i++)
+      {
+        const names=names[i];
+        const nameFirstLetters=names[0].upperCase();
+        const restName=names.slice(1);
+        const name=nameFirstLetters+restName;
+        names[i]=newName;
+       
+        
+      }
+      
+    return parts[0]+': '+names.join(', ')+' .';
+}
+
+console.log(correctSentence("Studentai: jonas, Maryte, peTras, ONA."));
+
+//case'.split(' ').map(s=>s.charAt(0).toUpperCase()+s.slice(1)).join(''));
+//console.log(`"camel case word" -->'`+'camel case word'.split(' ').map(s=>s.charAt(0).toUpperCase()+s.slice(1)).join(''));
+
+function camelCase(text) {
+    return text.split(' ').map(s=>s.charAt(0).toUpperCase()+s.slice(1)).join('');
+}
+console.log(`"camel case word" -->'`+'camel case word'.split(' ').map(s=>s.charAt(0).toUpperCase()+s.slice(1)).join(''));
+console.log(`"camel case" -->'`+'camel case'.split(' ').map(s=>s.charAt(0).toUpperCase()+s.slice(1)).join(''));
