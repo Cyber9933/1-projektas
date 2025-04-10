a88a68bdde52bfbde2b084c2ddf2e0859cf5a387
Lina <linate.pa@gmail.com>
2025-04-10 09:37:44 +0300

    destruk

--- JS/21-destrukturizavimas.js
+++ JS/21-destrukturizavimas.js
@@ -62,4 +62,151 @@ console.log(p213);
 const p4={d:4,...p3,c:99};
 console.log(p4);
 
+console.log('******PVZ*******');
 
+//1)
+const marks=[10,2,8,4,6];
+const [first, second,...rest]=marks;
+
+console.log(first);
+console.log(second);
+console.log(rest);
+
+console.log('--');
+//2)
+const names =['Jonas', 'Maryte', 'Petras','Ona'];
+const n1=names[0];
+const n2=names [1];
+const n34=names.slice(2)
+
+console.log(n1);
+console.log(n2);
+console.log(n34);
+
+console.log('--');
+
+// inesus destrukt (viskas paeiliui kas masyve, nors ir pakeiti vardus kitais pavadinimais)
+
+const[x,y,...z]=names;
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+const mix=[1,'asd',true,[],{},undefined]
+const [m1,m2,m3,...mRest]=mix;
+
+console.log(m1);
+console.log(m2);
+console.log(m3);
+console.log(mRest);
+
+console.log('--');
+
+// su OBJEKTAIS  (objektai neturi eiliskumo, renkiesi kas tau reikalinga)
+const person={
+    address:'Kaunas, Nenumo 8',
+    name:'Jonas',
+    age:99,
+    isMarried:true,
+}
+//skliaustai visada turi sutapti. Jei objektas {} skliaustai
+const {name, age,...p}=person;
+
+console.log(name);
+console.log(age);
+console.log(p);
+//istraukimas antru etapu naudoji pasirinkta kintamaji, siuo atveju 'p'
+//
+console.log(p.address);
+console.log(p.isMarried);
+
+console.log('--');
+//ciklas pvz
+
+const students=[
+    {name:'Jonas',age:99},
+    {name:'Maryte',age:88},
+    {name:'Petras',age:77},
+    {name:'Ona',age:66},
+];
+
+// Studentui, vardu Jonas, yra 99 metai.
+
+for(let i=0; i<students.length; i++){
+    const student=students[i];
+    const name=student.name;
+    const age=student.age;
+    const result=`Studentui, vardu ${name}, yra ${age} metai`
+    
+    console.log(result);
+    
+};
+
+
+console.log('--');
+
+
+//for of eina per objekus
+
+for(const student of students){
+    const name=student.name;
+    const age=student.age;
+    const result=`Studentui, vardu ${name}, yra ${age} metai`;
+    console.log(result);
+    
+}
+console.log('--');
+// destrukturizavimas inesamas
+
+for(const student of students){
+    const {name,age}=student;
+    //inestas destr)
+    const result=`Studentui, vardu ${name}, yra ${age} metai`;
+    console.log(result);
+    
+}
+console.log('----visiskai supaprastinta----');
+for(const{name,age}  of students){
+       const result=`Studentui, vardu ${name}, yra ${age} metai`;
+    console.log(result);
+    
+}
+console.clear();
+console.log('----number masyvas----');
+
+const numbers=[
+    [0],
+    [1,11],
+    [2,22,222],
+    [4],
+    [5],
+    [6],
+];
+//Pirmas skaicius yra X ir dar yra Y papildomu skaicius.
+
+for (const numberList of numbers){
+    const first=numberList[0];
+    const rest=numberList.slice(1);
+    const result=`Pirmas skaicius yra ${first} ir dar yra ${rest.length} papildomu skaicius`
+    console.log(result);
+    
+}
+console.log('******');
+
+
+for (const numberList of numbers){
+    const [first,...rest]=numberList;
+    const result=`Pirmas skaicius yra ${first} ir dar yra ${rest.length} papildomu skaicius`
+    console.log(result);
+    
+}
+
+console.log('***TRUMIAUSIAS VARIANTAS***');
+
+for (const [first,...rest] of numbers){
+    
+    const result=`Pirmas skaicius yra ${first} ir dar yra ${rest.length} papildomu skaicius`
+    console.log(result);
+    
+}
